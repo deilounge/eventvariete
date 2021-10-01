1555abd20bd99481cc5d7df94679d245c3d6ca15
Bartosz Arenin <bartosz.arenin@gmail.com>
2021-10-01 21:53:36 +0200

    New Event model version including extra fields for preview, localization, prices and longer types for text

--- src/main/java/pl/eventvariete/event/Event.java
+++ src/main/java/pl/eventvariete/event/Event.java
@@ -1,24 +1,25 @@
 package pl.eventvariete.event;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
-import pl.eventvariete.TypeOfEvent;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
 @Data
-public class Event {
+public class Event<List> {
 
     @Id
     @GeneratedValue(strategy =  GenerationType.IDENTITY)
     private Long id;
     private String name;
+    @Lob
+    private String preview;
+    @Lob
     private String description;
     private String city;
+    private String street;
+    private Float price;
     private LocalDateTime eventStartDate;
     private LocalDateTime eventEndDate;
     private String typeOfEvent;
